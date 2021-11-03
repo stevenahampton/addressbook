b806ebe3748e1496cf3ee054a8735b4f5c8f76ed
Steven Hampton <steven.hampton@monash.edu>
2021-11-04 10:13:18 +1100

    added comment

--- src/main/java/com/stevenhampton/addressbook/logging/LoggingFilter.java
+++ src/main/java/com/stevenhampton/addressbook/logging/LoggingFilter.java
@@ -14,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
+/**
+ * Log all incoming requests and outgoing responses.
+ */
 @Component
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
